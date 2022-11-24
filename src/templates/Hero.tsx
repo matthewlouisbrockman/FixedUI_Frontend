d93e4e968a41e16ca05561cf3e5f9966fe64ed0c
Matt Brockman <mattbrockman@gmail.com>
2022-11-23 20:25:42 -0800

    update home

--- src/templates/Hero.tsx
+++ src/templates/Hero.tsx
@@ -4,39 +4,22 @@ import { Background } from '../background/Background';
 import { Button } from '../button/Button';
 import { HeroOneButton } from '../hero/HeroOneButton';
 import { Section } from '../layout/Section';
-import { NavbarTwoColumns } from '../navigation/NavbarTwoColumns';
-import { Logo } from './Logo';
 
 const Hero = () => (
   <Background color="bg-gray-100">
-    <Section yPadding="py-6">
-      <NavbarTwoColumns logo={<Logo xl />}>
-        <li>
-          <Link href="https://github.com/ixartz/Next-JS-Landing-Page-Starter-Template">
-            <a>GitHub</a>
-          </Link>
-        </li>
-        <li>
-          <Link href="/">
-            <a>Sign in</a>
-          </Link>
-        </li>
-      </NavbarTwoColumns>
-    </Section>
-
     <Section yPadding="pt-20 pb-32">
       <HeroOneButton
         title={
           <>
-            {'The modern landing page for\n'}
-            <span className="text-primary-500">React developers</span>
+            {'Browse the web\n'}
+            <span className="text-primary-500">Without the pain</span>
           </>
         }
-        description="The easiest way to build a React landing page in seconds."
+        description="Browse websites the way that makes sense."
         button={
-          <Link href="https://creativedesignsguru.com/category/nextjs/">
+          <Link href="https://chrome.google.com/webstore/category/extensions">
             <a>
-              <Button xl>Download Your Free Theme</Button>
+              <Button xl>Download the Extension</Button>
             </a>
           </Link>
         }
