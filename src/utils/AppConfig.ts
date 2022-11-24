648426ac6e507f762f01fe4057ddd2e28b7b3610
Matt Brockman <mattbrockman@gmail.com>
2022-11-23 20:27:39 -0800

    update config

--- src/utils/AppConfig.ts
+++ src/utils/AppConfig.ts
@@ -1,6 +1,6 @@
 export const AppConfig = {
-  site_name: 'Starter',
-  title: 'Nextjs Starter',
-  description: 'Starter code for your Nextjs Boilerplate with Tailwind CSS',
+  site_name: 'Fixed UI',
+  title: 'Fix the interface for the web',
+  description: 'Browse the web without the annoyances',
   locale: 'en',
 };
